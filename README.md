e69f284e1b563cfa1e3f161486afdd0f5f3b503c
Your Name <ankitsaikia19@gmail.com>
2022-08-21 10:26:43 +0530

    update for abouts

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Portfolio
- Portfolio webage settings and others
+ Portfolio webpage created for detailed view on my experience as a web developer.
+ There are many other side projects that are available within the git repository.
+
+ ## Do not forget to leave a good feedback if you copy the source code!
+
+ ``
+ Things left to do:
+ 1. Make css stylesheet for devices of different screen sizes,
+ 2. Add animations according to the user's view [DYNAMIC VIEW],
+ 3. Add comments within the document.
--- js/typed.js
+++ js/typed.js
@@ -1,17 +1,24 @@
 (function ($) {
 	"use strict";
-	var nav = $('nav');
-  var navHeight = nav.outerHeight();
   
 	/*--/ Star Typed /--*/
 	if ($('.text-slider').length == 1) {
     var typed_strings = $('.text-slider-items').text();
 		var typed = new Typed('.text-slider', {
 			strings: typed_strings.split(','),
-			typeSpeed: 50,
+			typeSpeed: 60,
 			loop: true,
 			backDelay: 1100,
-			backSpeed: 40
+			backSpeed: 30
 		});
 	}
+
+	  // Preloader
+	  $(window).on('load', function () {
+		if ($('#preloader').length) {
+		  $('#preloader').delay(100).fadeOut('slow', function () {
+			$(this).remove();
+		  });
+		}
+	  });
 })(jQuery);
